39d4e2490570c07ac51d36c231b74e370361dede
davidrv <davidramirezvillegas@gmail.com>
2023-02-09 22:05:00 -0500

    Post method implemented

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -34,9 +34,28 @@ myDB(async client => {
   app.route('/').get((req, res) => {
     res.render('index', {
       title: 'Connected to Database',
-      message: 'Please log in'
+      message: 'Please log in',
+      showLogin: true
     });
   });
+
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  })
+
+  app.route('/profile').get((req, res) => {
+    res.render('profile');
+  })
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }));
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -47,16 +66,6 @@ myDB(async client => {
       done(null, doc);
     });
   });
-
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
-      return done(null, user);
-    });
-  }));
 
 }).catch(e => {
   app.route('/').get((req, res) => {
